1be2640ae195d907dd8dc260d99d42fa410492fe
Waridley <Waridley64@gmail.com>
2025-03-24 19:34:59 -0500

    Improve readme example

--- README.md
+++ README.md
@@ -12,9 +12,13 @@ use breaking_attr::breaking;
 fn hash_impl_did_not_change() {
     #[breaking(sha2 = "zEk8F98i1LX-Rr070lCztGerzO1-qlLbosetY1UvSww=")]
     const SEED: &str = "This value must not change between minor versions.";
-    #[breaking("ipCxMeWK-ImjLQVZX7YQXqjguSC5jOc5oCexXl5Bw0g=")] // Hasher defaults to `blake3`
-    const HASH: &[u8] = &[89, 216, 231, 42, 61, 80, 228, 91, 200, 17, 218, 190, 61, 187, 1, 13, 90, 195, 84, 57, 249, 109, 90, 79, 175, 103, 37, 195, 98, 32, 157, 102];
-    let hash = blake3::hash(SEED.as_bytes()); // just for example
-    debug_assert_eq!(hash.as_bytes(), HASH);
+
+    #[breaking("3zIKlGMq-shMmThdYSOntWhl9QCx3A23i8tnEfNPWBY=")] // defaults to `blake3`
+    const HASH: u64 = 42;
+    
+    // Just for example:
+    let mut hasher = std::hash::DefaultHasher::new();
+    hasher.write(SEED.as_bytes());
+    debug_assert_eq!(hasher.finish(), HASH);
 }
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -6,18 +6,20 @@
 //!
 //! ```
 //! use breaking_attr::breaking;
-//!
-//! # use blake3::{hash, Hash};
+//! # use std::hash::Hasher;
 //!
 //! #[cfg_attr(test, test)]
 //! fn hash_impl_did_not_change() {
 //!     #[breaking(sha2 = "zEk8F98i1LX-Rr070lCztGerzO1-qlLbosetY1UvSww=")]
 //!     const SEED: &str = "This value must not change between minor versions.";
-//!     #[breaking("ipCxMeWK-ImjLQVZX7YQXqjguSC5jOc5oCexXl5Bw0g=")] // Hasher defaults to `blake3`
-//!     const HASH: &[u8] = &[89, 216, 231, 42, 61, 80, 228, 91, 200, 17, 218, 190, 61, 187, 1, 13, 90, 195, 84, 57, 249, 109, 90, 79, 175, 103, 37, 195, 98, 32, 157, 102];
 //!
-//!     let hash = hash(SEED.as_bytes());
-//!     debug_assert_eq!(hash.as_bytes(), HASH);
+//!     #[breaking("cD1v24qkrBpNJl2awl4hTkYqrOHy3L3_IKMQSjN_LXo=")] // defaults to `blake3`
+//!     const HASH: u64 = 5201689092505688044;
+//!
+//!     // Just for example:
+//!     let mut hasher = std::hash::DefaultHasher::new();
+//!     hasher.write(SEED.as_bytes());
+//!     debug_assert_eq!(hasher.finish(), HASH);
 //! }
 //!
 //! # hash_impl_did_not_change()
