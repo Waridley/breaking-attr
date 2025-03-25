1c1dcdef29ccc71a83342e636bccc7f26bc36423
Waridley <Waridley64@gmail.com>
2025-03-24 19:04:49 -0500

    Format and bump version

--- tests/tests.rs
+++ tests/tests.rs
@@ -16,22 +16,24 @@ fn all_hashers() {
 
     #[breaking(sha256 = "yFGGvXNrkGi9W6EkurKoaR-YVX-wkVrvd3OLflrGyG0=")]
     pub const SHA256: &str = "sha256";
-    
+
     #[breaking(sha224 = "eDkZAHeOIImW3JGjkwEiOzCEy-JIeXgkAxE0gg==")]
     pub const SHA224: &str = "sha224";
-    
+
     #[breaking(sha384 = "dUZc1MuQAoTLrSKVuK1ovEIdz_orTO4QbGvUWOevtQ8SwyqlM-8_ppWwoUzOaC82")]
     pub const SHA384: &str = "sha384";
-    
-    #[breaking(sha512 = "q6pVPUidwbMP6dIZ7AkRtX1lI-BQ41T3B5xYl1d2-AuEyAgPcf3OrAukc5F4ipRbdNoHDmHJrh5pX0-Ax-RQFg==")]
+
+    #[breaking(
+        sha512 = "q6pVPUidwbMP6dIZ7AkRtX1lI-BQ41T3B5xYl1d2-AuEyAgPcf3OrAukc5F4ipRbdNoHDmHJrh5pX0-Ax-RQFg=="
+    )]
     pub const SHA512: &str = "sha512";
-    
+
     #[breaking(sha512_224 = "mnnxQ56r1rLGV66BDYBdwkP_No-pI0-GQfruQw==")]
     pub const SHA512_224: &str = "sha512_224";
-    
+
     #[breaking(sha512_256 = "Io7SHXF6BTzGjmPifLa58nSaz8bxdxXKoufjhoTPcQw=")]
     pub const SHA512_256: &str = "sha512_256";
-    
+
     #[breaking(md5 = "63_HcFESCMbwzC0-dDXfXA==")]
     pub const MD5: &str = "md5";
 }
